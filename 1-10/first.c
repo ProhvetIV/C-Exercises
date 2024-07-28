2c62f7d0db3b931692acce2da09fae19bb5a5779
Iisak <iisakvirma@gmail.com>
2024-07-28 20:15:49 +0300

    name

---
+++ 1-10/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Just Someone";
+    char dob[] = "10.10.2010";
+    char mobile[] = "10-1010-101";
+
+    printf("Name    : %s\n", name);
+    printf("DOB     : %s\n", dob);
+    printf("Mobile  : %s\n", mobile);
+}
